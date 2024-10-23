052b12e916a5b130b332118dabb64e667ddad90a
Siocic <faris.siocic@gmail.com>
2024-10-23 22:49:42 +0200

    change email configuration

--- eRekreacija/RegistrationEmailConsumer/EmailService.cs
+++ eRekreacija/RegistrationEmailConsumer/EmailService.cs
@@ -16,10 +16,9 @@ namespace EmailConsumer
         public void SendEmailForRegistration(string userEmail, string message)
         {
             var emailConfig = _configuration.GetSection("Email");
-            var emailMessage = new MimeMessage();
-            
-            var fromEmail = Environment.GetEnvironmentVariable("Email");
+            var emailMessage = new MimeMessage();            
 
+            var fromEmail = Environment.GetEnvironmentVariable("Email");
             emailMessage.From.Add(new MailboxAddress("Rekreacija Registration Email", fromEmail));
             emailMessage.To.Add(new MailboxAddress("Customer", userEmail));
             emailMessage.Subject = "Rekreacija application registration";
@@ -27,16 +26,15 @@ namespace EmailConsumer
             {
                 Text = message
             };
-
             using var client = new SmtpClient(); 
             var port=Environment.GetEnvironmentVariable("Port");
-            var host=Environment.GetEnvironmentVariable("Host");
+            var host=Environment.GetEnvironmentVariable("Host");        
             try
             {
                 client.Connect(host, int.Parse(port), false);
                 var pass = Environment.GetEnvironmentVariable("Password");
                 var user = Environment.GetEnvironmentVariable("EmailUsername");
-
+              
                 client.Authenticate(user, pass);
                 client.Send(emailMessage);
             }
