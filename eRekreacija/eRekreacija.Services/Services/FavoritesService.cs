872a4fe3d2311765d84a1fbe941f73d92bc9a58f
Siocic <faris.siocic@gmail.com>
2025-03-25 21:42:07 +0100

    fix favorites

--- eRekreacija/eRekreacija.Services/Services/FavoritesService.cs
+++ eRekreacija/eRekreacija.Services/Services/FavoritesService.cs
@@ -4,11 +4,29 @@ using eRekreacija.Models.Models;
 using eRekreacija.Services.Database.Context;
 using eRekreacija.Services.Database.Entities;
 using eRekreacija.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace eRekreacija.Services.Services
 {
-    public class FavoritesService:BaseCRUDService<tbl_Favorites,FavoritesDTO,FavoritesInsertRequest,FavoritesUpdateRequest>,IFavoritesService
+    public class FavoritesService : BaseCRUDService<tbl_Favorites, FavoritesDTO, FavoritesInsertRequest, FavoritesUpdateRequest>, IFavoritesService
     {
-        public FavoritesService(RekreacijaContext rekreacijaContext,IMapper mapper):base(rekreacijaContext,mapper){}
+        public FavoritesService(RekreacijaContext rekreacijaContext, IMapper mapper) : base(rekreacijaContext, mapper) { }
+
+        public override async Task<FavoritesDTO> Insert(FavoritesInsertRequest model)
+        {
+            var checkIsAlreadyFavorite = await _rekreacijaContext.Set<tbl_Favorites>().FirstOrDefaultAsync(s => s.user_id == model.user_id && s.object_id == model.object_id);
+            if (checkIsAlreadyFavorite != null)
+            {
+                _rekreacijaContext.Remove(checkIsAlreadyFavorite);
+                await _rekreacijaContext.SaveChangesAsync();
+                return null;
+            }
+
+            var entity = _mapper.Map<tbl_Favorites>(model);
+            await _rekreacijaContext.Set<tbl_Favorites>().AddAsync(entity);
+            await _rekreacijaContext.SaveChangesAsync();
+            return _mapper.Map<FavoritesDTO>(entity);
+
+        }
     }
 }
