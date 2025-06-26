bd40949e2c570d2e27ee048dcfdd7b61acd022c1
Siocic <faris.siocic@gmail.com>
2025-06-26 08:12:48 +0200

    add column number of players in appointment

--- eRekreacija/eRekreacija.Services/Database/Entities/tbl_Appointment.cs
+++ eRekreacija/eRekreacija.Services/Database/Entities/tbl_Appointment.cs
@@ -6,6 +6,7 @@
         public DateTime? appointment_date { get; set; }
         public DateTime? start_time { get; set; }
         public DateTime? end_time { get; set; }
+        public int? number_of_players { get; set; }
         public bool? is_approved { get; set; } = null;
         public tbl_Objects TblObjects { get; set; }
         public int object_id { get; set; }
---
+++ eRekreacija/eRekreacija.Services/Migrations/Rekreacija/20250626060838_addNumberOfPlayersToAppointment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eRekreacija.Services.Migrations.Rekreacija
+{
+    /// <inheritdoc />
+    public partial class addNumberOfPlayersToAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "number_of_players",
+                table: "tbl_Appointment",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "number_of_players",
+                table: "tbl_Appointment");
+        }
+    }
+}
