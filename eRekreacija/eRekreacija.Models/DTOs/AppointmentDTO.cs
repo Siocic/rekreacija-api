198551fe6ed62405da58bc5d9b054cde2ca25042
Ishak Isabegovic <ishak.isabegovic@edu.fit.ba>
2025-06-26 10:26:05 +0200

    add number of players to appointment

--- eRekreacija/eRekreacija.Models/DTOs/AppointmentDTO.cs
+++ eRekreacija/eRekreacija.Models/DTOs/AppointmentDTO.cs
@@ -6,6 +6,7 @@
         public DateTime? appointment_date { get; set; }
         public DateTime? start_time { get; set; }
         public DateTime? end_time { get; set; }
+        public int? number_of_players { get; set; }
         public bool? is_approved { get; set; } = false;
         public int object_id { get; set; }
         public string user_id { get; set; }
